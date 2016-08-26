724e2cd125b719c90a49362e10c21ce8192534ab
ehzShelter <emrulhasanzawad@outlook.com>
2016-08-26 20:31:11 +0600

    exploring the beauty of python

--- mario.py
+++ mario.py
@@ -1,6 +1,4 @@
-n = 10
-
-def mario():
+def mario(n=2):
     for row in range(n):
         for col in range(n):
             if row + col < n - 1:
@@ -10,4 +8,22 @@ def mario():
         print('#')
 
 
-mario()
+# mario(10)
+
+# If you want to call like C
+if __name__ == '__main__':
+    mario(10)
+
+# python magic
+print('\n')
+print('%' * 5)
+print('#' * 7)
+print('marioMagic Implementaton')
+
+
+def marioMagic(n=10):
+    # remember row indexing start from zero
+    for row in range(n):
+        print((' ' * (n - row - 1)) + ('#' * row) + '##')
+
+marioMagic(15)
